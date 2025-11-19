def2b27b97723c404d4cd80ce774dac85a370780
brenhen <brennahen3@gmail.com>
2025-11-19 13:37:12 -0500

    Fixing Terrain.h, added more. TODOS

--- map/Terrain.h
+++ map/Terrain.h
@@ -12,13 +12,45 @@ using std::vector, glm::vec3, glm::mat4;
 
 // OUTLINE / TODOS
 
-// 1. Store a reference to the shader used to draw the cube
+// 1. Store a reference to the shader
 // 2. Store terrain dimensions and position
 // 3. Store a vector of Cube objects
 // 4. Build a grid of cubes - flat for now until it works, then add 3D modeling later
 // 5. Draw all cubes using the shader
 
+// Store a reference to the shader
+struct color
+{
+    // RGBY colors but can change later
+    float red;
+    float green;
+    float blue;
+    float yellow;
 
+};
+
+// Store terrain dimensions and position
+class Terrain
+{
+public:
+    Terrain(Shader &shader, int width, int height, float cellSize);
+    void draw(const glm::mat4 &view, const glm::mat4 &projection) const;
+private:
+    Shader shader;
+    int width;
+    int height;
+    float cellSize;
+    glm::mat4 model;
+    vector<Cube> tiles;
+    void initTiles();
+};
+
+// TODO: Store vector of cube objects
+
+
+// TODO: Build a grid of cubes- flat for now until it works, then add 3D modeling later
+
+// TODO: Draw all cubes using the shader
 
 
 
